5d84b7e90caf0fd3f308747c08c0406da6d7010a
Roman Tkachenko <rt4freel@gmail.com>
2021-10-03 00:48:02 +0300

    Third task

---
+++ task3/Flat.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Text;
+
+namespace sigma_t3
+{
+    class Flat
+    {
+        // kWt values to store per quarter
+        public const int VALUES = 4;
+
+        private int current;
+        private float[] vals = new float[VALUES];
+        public int Quarter { get; set; }
+        public int Number { get; private set; }
+        public string Owner { get; set; }
+        public int CurrentMonth
+        {
+            get { return current; }
+        }
+        public float Total 
+        {
+            get
+            {
+                return this.vals[current - 1] - this.vals[0];
+            }
+        }
+
+        public Flat() : this(0, "", 0.0f) { }
+
+        public Flat(int number, string owner, params float[] kWt)
+        {
+            this.Quarter = 1;
+            this.Number = number;
+            this.Owner = owner;
+            this.current = kWt?.Length < VALUES ? kWt.Length : VALUES;
+            for(int i = 0; i < current; i++)
+            {
+                this.vals[i] = kWt[i];
+            }
+        }
+
+        public float this[int index]
+        {
+            get
+            {
+                if (index >= current)
+                    return 0.0f;
+                return this.vals[index];
+            }
+        }
+
+        public void EndMonth(float kWt)
+        {
+            if (current < VALUES)
+                this.vals[current++] = kWt;
+        }
+
+        public float CurrentDebt(float price)
+        {
+            return price * this.Total;
+        }
+
+        //returns total kWt for quarter
+        public float EndQuarter()
+        {
+            float result = this.Total;
+            this.vals[0] = this.vals[current - 1];
+            for(int i = 1; i < current; i++)
+            {
+                this.vals[i] = 0.0f;
+            }
+            current = 1;
+            return result;
+        }
+
+        //returns debt for current quarter
+        public float EndQuarter(float price) { return price * this.EndQuarter(); }
+
+        public string ToShortString()
+        {
+            return Number + ", " + Owner + ", " + string.Join(", ", vals);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Flat #" + Number + " belongs to " + Owner);
+            DateTime month;
+            for(int i = 0; i < VALUES - 1; i++)
+            {
+                if (vals[i + 1] < vals[i]) break;
+                month = new DateTime(1971, (Quarter - 1) * 4 + i + 1, 1);
+                sb.AppendLine(String.Format("\t{0}: {1:0.00} - {2:0.00} = {3:0.00} kWt", 
+                    month.ToString("MMMM"), vals[i + 1], vals[i], vals[i + 1] - vals[i]));
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ task3/QuarterReport.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace sigma_t3
+{
+    
+    class QuarterReport
+    {
+        public int Quarter { get; private set; }
+        public int Amount { get { return flats.Length; } }
+        private Flat[] flats;
+
+        public QuarterReport() : this(1, 10) { }
+
+        public QuarterReport(string path)
+        {
+            using (StreamReader file = new StreamReader(path))
+            {
+                string[] args = file.ReadLine().Trim().Split(" ");
+                flats = new Flat[int.Parse(args[0])];
+                Quarter = int.Parse(args[1]);
+                for(int i = 0; i < flats.Length; i++)
+                {
+                    args = file.ReadLine().Split(",");
+                    flats[i] = new Flat(int.Parse(args[0]),
+                                        args[1].Trim(),
+                                        float.Parse(args[2]));
+                    flats[i].Quarter = Quarter;
+                    for(int j = 3; j < args.Length; j++)
+                    {
+                        if(float.Parse(args[j]) != 0)
+                            flats[i].EndMonth(float.Parse(args[j]));
+                    }
+                }
+            }
+        }
+
+        private QuarterReport(int quarter, int amount)
+        {
+            this.flats = new Flat[amount];
+        }
+
+        public Flat this[int index]
+        {
+            get
+            {
+                if(index < this.Amount)
+                    return flats[index];
+                return null;
+            }
+            set
+            {
+                if (index < this.Amount)
+                    this.flats[index] = value;
+            }
+        }
+
+        public static QuarterReport Random(int quarter, int amount)
+        {
+            Random rand = new Random();
+            float kWt = (float)rand.NextDouble() * 1000.0f;
+            QuarterReport result = new QuarterReport(quarter, amount);
+            result.Quarter = quarter;
+            for (int i = 0; i < amount; i++)
+            {
+                result[i] = new Flat(i + 1, "Owner " + (i + 1), kWt);
+                result[i].Quarter = quarter;
+                for (int j = 0; j < Flat.VALUES - 1; j++)
+                {
+                    kWt += (float)rand.NextDouble() * 100;
+                    result[i].EndMonth(kWt);
+                }
+            }
+            return result;
+        }
+
+        public Flat GetMaxDebt(float price = 1.0f)
+        {
+            int result = 0;
+            float max = flats[0].CurrentDebt(price);
+            float cur;
+            for(int i = 1; i < Amount; i++)
+            {
+                if((cur = flats[i].CurrentDebt(price)) > max)
+                {
+                    result = i;
+                    max = cur;
+                }
+            }
+            return flats[result];
+        }
+
+        public Flat GetEmptyFlat()
+        {
+            for (int i = 1; i < Amount; i++)
+            {
+                if (flats[i].Total == 0.0f)
+                {
+                    return flats[i];
+                }
+            }
+            return null;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine(String.Format("Report about {0} quarter from {1} flats",
+                Quarter, Amount));
+            for(int i = 0; i < Amount; i++)
+            {
+                sb.Append(flats[i].ToString() + (i < Amount - 1 ? "\n" : ""));
+            }
+            return sb.ToString();
+        }
+
+        public void ToFile(string path)
+        {
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                writer.WriteLine(Amount + " " + Quarter);
+                for (int i = 0; i < Amount; i++)
+                {
+                    writer.Write(flats[i].ToShortString() + (i < Amount - 1 ? "\n" : ""));
+                }
+            }
+        }
+    }
+}
